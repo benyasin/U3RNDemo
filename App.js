f5c51f6f11f3e4e2e7586257a9eaac6d0b6f00ab
benyasin <rgwapkk520>
2019-07-17 21:00:29 +0800

    update

--- App.js
+++ App.js
@@ -1,56 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import { createU3, U3Utils } from 'u3.js'
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { createU3, U3Utils } from 'u3.js';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-})
+    'Shake or press menu button for dev menu',
+});
 
-type
-Props = {}
-export default class App extends Component<Props> {
+export default class App extends Component {
 
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      chainInfo: null
-    }
+      chainInfo: null,
+    };
   }
 
-  async componentDidMount () {
-    const u3 = createU3()
-    await u3.getChainInfo((err, info) => {
-      if (err) throw err
-      this.setState({
-        chainInfo: JSON.stringify(info)
-      })
-    })
+  async componentDidMount() {
+    const u3 = createU3();
+    let info = await u3.getChainInfo();
+    this.setState({
+      chainInfo: JSON.stringify(info),
+    });
 
-    let result = U3Utils.ecc.generateKeyPairWithMnemonic()
-    console.log(result)
+    let result = U3Utils.ecc.generateKeyPairWithMnemonic();
+
+    console.log(result);
   }
 
-  render () {
+  render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to U3RNDemo!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-
-        <Text style={styles.chainInfo}>{this.state.chainInfo}</Text>
+        <Text style={styles.instructions}>getChainInfo</Text>
+        <Text>{this.state.chainInfo}</Text>
       </View>
-    )
+    );
   }
 }
 
@@ -59,23 +46,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10
+    margin: 10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5
+    marginBottom: 5,
   },
   chainInfo: {
     marginTop: 30,
     textAlign: 'left',
     color: '#875643',
     marginBottom: 5,
-    marginHorizontal: 20
-  }
-})
+    marginHorizontal: 20,
+  },
+});
